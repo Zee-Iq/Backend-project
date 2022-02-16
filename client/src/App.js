f21471cf942fe23b6d5bbd3d6f3271681be779c0
kimdev <aa171@students.kiron.ngo>
2022-02-16 14:01:17 +0100

    add some stuuf and remove some stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 function App() {
-  return <div className="App">
-    <h1>hello from app.js</h1>
-  </div>;
+  return (
+    <div className="App">
+      <h1>hello from app.js</h1>
+    </div>
+  );
 }
 
 export default App;
