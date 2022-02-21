851d86badb2fe2ece4ce89dc184f185b0bd4cf8f
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-21 14:49:48 +0100

    imported register and login

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,18 @@ import {Contact}  from "./pages/Contact/Contact";
 import { Route, Switch } from "react-router-dom";
 import Admin from "./pages/Admin/admin";
 import LogIn from "./components/RegisterLogin/login";
+import Register from "./components/RegisterLogin/register";
 function App() {
   return (
     <div className="App">
      
       <Switch>
-         <LogIn />
+        
+        {/*  <Register />
+         <LogIn /> */}
         <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={LogIn} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/admin' component={Admin} />
       </Switch>
