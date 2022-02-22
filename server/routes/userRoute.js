1c79b1382d9354c4f4a06d5039626decc17e8c8a
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-22 16:25:56 +0100

    created userRoute.js

---
+++ server/routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userController = require('../controllers/userController')
+
+router.use(express.json())
+
+// console.log('router is', router)
+
+router.get('/list', userController.handleListUser)
+// router.post('/add', usersController.handleAddUser)
+// router.get('/delete', usersController.handleAddUser)
+// router.get('/update', usersController.handleAddUser)
+
+module.exports = router
