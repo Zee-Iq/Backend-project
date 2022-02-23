9bbd9e42a9f8194ae439c4a4414997810ba72d9e
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-23 11:45:45 +0100

    added handleAddUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,3 +3,7 @@ const user = require()
 exports.handleListUser = (req,res) =>{
     res.send("Hello from userController")
 }
+/* 
+exports.handleAddUser = (req,res) => {
+    res.send("Hello from AddUser")
+} */
