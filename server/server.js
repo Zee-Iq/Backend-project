4174847dba255af5fc21692379cef59df0286810
kemdev <kareem_kemo@live.com>
2022-07-16 04:26:40 +0200

    update packagejson to correctly work on heroku

--- server/server.js
+++ server/server.js
@@ -18,3 +18,34 @@ app.use('/users', require('./routes/registerRoute'))
 
 const port = process.env.PORT || 8000
 app.listen(port,() => console.log("server is running at " + port) )
+
+
+
+// heroku stuff
+app.use(express.static("client/build"));
+
+// test https for heroku app
+// app.use(enforce.HTTPS());
+// enforce.HTTPS({ trustProtoHeader: true });
+// http.createServer(app).listen(port, function () {
+//   console.log("Express server listening on port " + port);
+// });
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+  );
+}
